b54e29131ad0e5a07e25ce562c0902522ee191ac
stes <stes94@ymail.com>
2012-07-23 22:09:23 +0200

    html parser

--- src/nbot/htmlparser.py
+++ src/nbot/htmlparser.py
@@ -4,14 +4,33 @@ Created on 23.07.2012
 @author: stes
 '''
 
-class HTMLParser():
-    '''
-    classdocs
-    '''
+import re
+import httplib
 
+def fetch_content(url):
+    conn = httplib.HTTPConnection(url)
+    conn.request("GET", "/index.html")
+    response = conn.getresponse()
+    content = response.read()
+    conn.close()
+    return content
 
-    def __init__(self):
-        '''
-        Constructor
-        '''
-        
+def remove_tags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+def remove_spaces(data):
+    p = re.compile(r'\s+')
+    return p.sub(' ', data)
+
+content = fetch_content("python.org")
+i = 0
+tmp = ''
+for c in remove_spaces(remove_tags(content)):
+    tmp+=c
+    if i > 80:
+        print tmp
+        tmp = ''
+        i = 0
+    else:
+        i+=1
