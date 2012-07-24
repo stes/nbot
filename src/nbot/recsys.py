6df0991d8c7dfa1fdc3f9eace2a2534ba39dc3e5
stes <stes94@ymail.com>
2012-07-24 18:15:24 +0200

    work on vocab list

--- src/nbot/recsys.py
+++ src/nbot/recsys.py
@@ -4,9 +4,9 @@ Created on 23.07.2012
 @author: stes
 '''
 
+from htmlparser import *
 from numpy import *
 from numpy.linalg import *
-from htmlparser import *
 
 def preprocess(text):
     '''
@@ -16,6 +16,7 @@ def preprocess(text):
     p = re.compile(r'<script.*?</script>', re.DOTALL)
     text = p.sub('', text)
     p = re.compile(r'<a href.*?</a>', re.DOTALL)
+    #TODO use some better name for this
     text = p.sub('hyperlinkk', text)
     text = remove_spaces(remove_tags(text))
     text = text.lower()
@@ -58,24 +59,37 @@ class RecommenderSystem():
 
 class VocabList(): 
     '''
-    dictionary to store all possible words
+    Vocabulary list used to create a 'bag of words' model out of a given text document.
     '''   
     
     def __init__(self):
         '''
-        Constructor
+        Constructs a new, empty vocabulary list
         '''
         self.__dict = dict()
     
     def expand_with(self, text):
         '''
         expands the vocabulary list using the specified (preprocessed) text
+        @param text: the text whose words should be added to the vocabulary list
         '''
         for word in text.split():
             if self.__dict.has_key(word):
                 self.__dict[word] += 1
             else:
                 self.__dict[word] = 1
+    
+    def clean(self, threshold):
+        '''
+        removes all entries with a quantity less or equals the threshold value
+        @param threshold: the threshold
+        '''
+        rmv = []
+        for item in self.__dict:
+            if (self.__dict[item] <= threshold):
+                rmv.append(item)
+        for item in rmv:
+            self.__dict.pop(item)
     
     def __str__(self):
         string = ''
@@ -84,6 +98,7 @@ class VocabList():
         return string
     
 if __name__ == '__main__':
+    # some tests
     content = fetch_content('codinghorror.com', '/blog')
     content = preprocess(content)
     tmp = ''
@@ -94,4 +109,5 @@ if __name__ == '__main__':
             tmp = ''
     vlist = VocabList()
     vlist.expand_with(content)
+    vlist.clean(10)
     print vlist
